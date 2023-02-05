1a8d795bac47fe37d21d8ba4768261124e12c7c4
Mikalai Shchurko <maxe1.first@gmail.com>
2023-02-05 15:57:37 +0600

    Updating url for weave scope
    apply fails because cloud.weave.works reached his end of life:
    https://www.weave.works/blog/weave-cloud-end-of-service
    https://github.com/weaveworks/weave/issues/3960

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ kubeadm init --apiserver-advertise-address $(hostname -i)
 mkdir -p $HOME/.kube
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -n kube-system -f \
-    "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 |tr -d '\n')"
+    "https://github.com/weaveworks/scope/releases/download/v1.13.2/k8s-scope.yaml"
